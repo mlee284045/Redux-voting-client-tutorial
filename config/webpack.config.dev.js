77552d49e6d85efe6869798af12de7e6d47f731b
mlee-nexient <mlee@nexient.com>
2017-02-27 17:10:23 -0500

    difficult to configure create-react=app to follow the tutorial

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -38,9 +38,9 @@ module.exports = {
     // Note: instead of the default WebpackDevServer client, we use a custom one
     // to bring better experience for Create React App users. You can replace
     // the line below with these two lines if you prefer the stock client:
-    // require.resolve('webpack-dev-server/client') + '?/',
-    // require.resolve('webpack/hot/dev-server'),
-    require.resolve('react-dev-utils/webpackHotDevClient'),
+    require.resolve('webpack-dev-server/client') + '?/http://localhost:3000',
+    require.resolve('webpack/hot/only-dev-server'),
+    // require.resolve('react-dev-utils/webpackHotDevClient'),
     // We ship a few polyfills by default:
     require.resolve('./polyfills'),
     // Finally, this is your app's code:
@@ -79,7 +79,10 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -117,14 +120,14 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
-        loader: 'babel',
-        query: {
+        loader: 'react-hot!babel'
+        // query: {
           
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
-          cacheDirectory: true
-        }
+        //   cacheDirectory: true
+        // }
       },
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
